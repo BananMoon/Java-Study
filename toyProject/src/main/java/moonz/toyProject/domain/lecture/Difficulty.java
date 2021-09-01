6e6f1f2363afe25be4049b7eb0050aa4ce2a145d
moonz <myj7082@naver.com>
2021-09-01 19:33:59 +0900

    Delete toyProject directory

--- toyProject/src/main/java/moonz/toyProject/domain/lecture/Difficulty.java
+++
@@ -1,5 +0,0 @@
-package moonz.toyProject.domain.lecture;
-
-public enum Difficulty {
-    BEGGINER, INTER, ADVANCED
-}
--- toyProject/src/main/java/moonz/toyProject/domain/user/Role.java
+++
@@ -1,5 +0,0 @@
-package moonz.toyProject.domain.user;
-
-public enum Role {
-    GENERAL_USER, SHARER
-}
