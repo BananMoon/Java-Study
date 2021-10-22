d8a3f64da4a343d7999870bd79ecdc325b525bf9
moonz <banan99@naver.com>
2021-10-22 23:46:02 +0900

    진행중:Effective Java code - Item2 추가

---
+++ Effective Java_book/Effective-Java-code/src/Item2/NyPizza.java
@@ -0,0 +1,26 @@
+package Item2;
+
+import java.util.Objects;
+
+// Pizza의 하위 클래스
+public class NyPizza extends Pizza {
+    public enum Size { SMALL, MEDIUM, LARGE}
+    private final Size size;
+
+    public static class Builder extends Pizza.Builder<Builder> {
+        private final Size size;
+        public Builder(Size size) {
+            this.size = Objects.requireNonNull(size);
+        }
+
+        @Override public NyPizza build() {
+            return new NyPizza(this);
+        }
+        @Override protected Builder self() { return this;}
+    }
+
+    private NyPizza (Builder builder) {
+        super(builder);
+        size = builder.size;
+    }
+}
