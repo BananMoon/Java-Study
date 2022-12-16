ffcbae34686ab06211a89bfa0a9fd857cdfdbff8
BananMoon <younji1115@gmail.com>
2022-12-16 09:43:00 +0900

    feat: ch10-10. 스트림에서의 순위 문제 풀이 코드(이진 트리 이용)

---
+++ Java-Practice/src/main/java/com/moonz/javapractice/CrackingCodingInterview/ch10/prob10/RankNode.java
@@ -0,0 +1,55 @@
+package com.moonz.javapractice.CrackingCodingInterview.ch10.prob10;
+
+public class RankNode {
+    RankNode left, right;
+    int data;
+    int leftSize = 0;
+    public RankNode (int d) {
+        data = d;
+    }
+
+    /**
+     * 스트림으로 읽은 수를 트리에 삽입
+     * 이때 this는 루트이다.
+     */
+    public void insert(int d) {
+        if (this.data < d) {    // 오른쪽 삽입
+            if (this.right != null) {
+                this.right.insert(d);
+            } else {
+                this.right = new RankNode(d);
+            }
+        } else {    // 기준 노드보다 같거나 작은 경우, 왼쪽 삽입
+            if (this.left != null) {
+                this.left.insert(d);
+            } else {
+                this.left = new RankNode(d);
+            }
+            leftSize++;  // 기준 노드보다 작은 값이 들어오면 카운터 증가!
+        }
+    }
+
+    /**
+     * x의 랭킹 (x보다 같거나 작은 수의 개수, 자신 제외)을 반환한다.
+     */
+    public int getRank (int d) {
+        if (this.data == d) {
+            return this.leftSize;
+        } else if (this.data < d) { /* 현재 노드 값보다 큰 값이면 */
+            int rightSize = this.right == null? -1 : this.right.getRank(d); // 값이 없다면 -1 반환 처리!!! 아니라면 오른쪽 트리로 이동
+            /* 현재 노드의 오른쪽 트리 중 d를 찾으면서 계산한 값 right_total에 대한 체크 */
+            if (rightSize == -1) {  // 값을 발견하지 못한 경우
+                return -1;
+            } else {    // 자신의 왼쪽 트리 크기 + 본인 1 + 오른쪽 트리에서의 값 d 찾기까지의 크기
+                return this.leftSize + 1 + rightSize;
+            }
+
+        } else {    /* 현재 노드 값보다 작은 값이면 */
+            if (this.left == null) {    // 찾는 값이 없으면!
+                return -1;
+            } else {
+                return this.left.getRank(d);    // 현재 노드의 왼쪽 트리의 랭크 재귀 호출
+            }
+        }
+    }
+}
---
+++ Java-Practice/src/main/java/com/moonz/javapractice/CrackingCodingInterview/ch10/prob10/prob10.java
@@ -0,0 +1,36 @@
+package com.moonz.javapractice.CrackingCodingInterview.ch10.prob10;
+
+/** 스트림에서의 순위
+ * - 순위? x보다 작거나 같은 수들의 갯수
+ * - 방법
+ * 1. 값이 정렬되어있다면 getRankOfNumber()는 효율적이지만, track()은 매 값을 추가할 때마다 원소들을 옮겨야해 비효율적이다.
+ * 2(selected). 이진 탐색 트리 : 원소 간 상대적 순서를 유지하면서 새로운 값이 들어와도 효율적으로 배치되는 자료 구조
+ */
+public class prob10 {
+    static RankNode root = null;
+    public static void main(String[] args) {
+//        int[] inputStream = {5, 1, 4, 4, 5, 9, 7, 13, 3};
+        int[] inputStream = {20, 15, 25, 10, 23, 5, 13, 24};
+        for (int s : inputStream) {
+            track(s);
+        }
+        System.out.println(getRankOfNumber(24)); // 6위
+        System.out.println(getRankOfNumber(20)); // 4위
+        System.out.println(getRankOfNumber(5));  // 0위
+    }
+
+    static public int getRankOfNumber(int x)  {
+        return root.getRank(x);
+    }
+    /**
+     * 스트림의 수를 한개 읽을 때마다 호출되어 랭크에 추가한다.
+     * 원소 간 상대적 순서 유지하면서 새 원소 삽입하기에 효율적인 자료구조 => 이진 탐색 트리
+     */
+    static void track (int x) {
+        if (root == null) {
+            root = new RankNode(x);
+        } else {
+            root.insert(x); // root 기준으로 데이터 검색 후 적절한 곳에 삽입
+        }
+    }
+}
